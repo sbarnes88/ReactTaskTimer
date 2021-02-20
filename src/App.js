7dd38fb49cfe46ee1163ca1b561c8c5d22f7c18c
sbarnes88 <65136741+sbarnes88@users.noreply.github.com>
2021-02-19 22:58:37 -0700

    Initial Commit
    Initial commit for React Task Timer

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Task from './Task';
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.addTask = this.addTask.bind(this);
+    this.state = {tasks: []};
+  }
+
+  addTask() {
+    console.log(this.state.tasks);
+    var newTaskList = this.state.tasks;
+    var task = new Task();
+    task['uuid'] = this.uuidv4();
+    newTaskList.push(task);
+    this.setState({tasks: newTaskList})
+  }
+
+  uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <table>
+            <tbody>
+          {this.state.tasks.map((t) => <Task key={t['uuid']}></Task>)}
+          </tbody>
+          </table>
+          <button onClick={this.addTask}>Add Task</button>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+/* function App() {
+const tasks = [];
+  return (
+    <div className="App">
+      <header className="App-header">
+        <table>
+          <thead>
+            <th>Name</th>
+            <th>Time</th>
+            <th></th>
+            <th></th>
+            <th></th>
+
+          </thead>
+          {tasks.map((task) => <Task></Task>)}
+        </table>
+        <button onClick={addTask}>Add Task</button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+} */
+
+export default App;
---
+++ src/Task.js
@@ -0,0 +1,82 @@
+import React from 'react'
+
+class Task extends React.Component {
+    timerId = -1;
+
+    constructor(props) {
+        super(props);
+        this.state = {time:0, taskName:'',priority:0,completed:false}
+        this.handleTaskNameChange = this.handleTaskNameChange.bind(this);
+        this.startTimer = this.startTimer.bind(this);
+        this.stopTimer = this.stopTimer.bind(this);
+        this.resetTimer = this.resetTimer.bind(this);
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+        this.stopTimer();
+    }
+
+    updateTime() {
+        this.setState({time: this.state.time+1});
+    }
+
+    startTimer() {
+        if(this.timerId != -1)
+            this.stopTimer();
+        this.timerId = setInterval(() => this.updateTime(), 1000);
+    }
+
+    stopTimer() {
+        if(this.timerId === -1)
+            return;
+        clearInterval(this.timerId);
+        this.timerId = -1;
+    }
+
+    resetTimer() {
+        this.stopTimer();
+        this.setState({time:0});
+    }
+
+    handleTaskNameChange(event) {
+        this.setState({taskName: event.target.value});
+    }
+
+    render() {
+        return(
+                    <tr>
+                        <td>
+                    <input type="text" value={this.state.taskName} onChange={this.handleTaskNameChange} />
+                    </td>
+                    <td>
+                        <code>{this.getFormattedTime()}</code>
+                    </td>
+                    <td><button onClick={this.startTimer}>Start</button></td>
+                    <td><button onClick={this.stopTimer}>Stop</button></td>
+                    <td><button onClick={this.resetTimer}>Reset</button></td>
+                   </tr>
+        );
+    }
+
+    getFormattedTime() {
+        var hours = Math.floor(this.state.time/60/60);
+        var minutes = Math.floor(this.state.time/60) - hours*60;
+        var seconds = this.state.time - (hours*60*60) - (minutes*60);
+        var appendMinutesZero = false;
+        var appendSecondsZero = false;
+
+        if(minutes < 10)
+            appendMinutesZero = true;
+        if(seconds < 10)
+            appendSecondsZero = true;
+        
+        return `${hours}:${(appendMinutesZero ? '0' : '')+minutes}:${(appendSecondsZero ? '0' : '')+seconds}`;
+    }
+}
+
+export default Task;
